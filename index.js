2e891533b36a8d066df2437433b6565b9184b1d3
RonaldBoc <78918415+RonaldBoc@users.noreply.github.com>
2021-08-02 17:14:08 -0400

    Changement d'image

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+$(document).ready(function() {
+ 
+  // The Url from your Authorisations list  
+  var instantTokenApiUrl = 'https://ig.instant-tokens.com/users/5e14ca15-94ff-4d25-a573-f0bf78f4204e/instagram/17841400605919626/token.js?userSecret=8svxn8lixx3nfuf2ckvsl'
+  
+	$.ajax({
+		  url: instantTokenApiUrl,
+      dataType: 'json'
+		})
+		 .done(function( response ) {
+          
+          if (!response.Token) {
+            console.log('Error :: ', response);
+          } else {
+              var feed = new Instafeed({
+                         accessToken: response.Token
+                        });
+              feed.run();
+            }
+  });
+ 
+});
